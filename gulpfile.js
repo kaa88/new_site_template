04f3669b3a0b61b1827cdc00328fffb0c93fa0bf
kaa88 <81631416+kaa88@users.noreply.github.com>
2021-08-01 20:47:24 +0300

    test font mixin

--- gulpfile.js
+++ gulpfile.js
@@ -118,3 +118,29 @@ exports.html = html;
 exports.build = build;
 exports.watch = watch;
 exports.default = watch;
+
+
+// добавить скрипт для шрифтов:
+// let fs = require('fs');
+// function fontsStyle() {
+// 	let file_content = fs.readFileSync(source_folder + '/scss/fonts.scss');
+// 	if (file_content == '') {
+// 		fs.writeFile(source_folder + '/scss/fonts.scss', '', cb);
+// 		return fs.readdir(path.build.fonts, function (err, items) {
+// 			if (items) {
+// 				let c_fontname;
+// 				for (var i = 0; i < items.length; i++) {
+// 					let fontname = items[i].split('.');
+// 					fontname = fontname[0];
+// 					if (c_fontname != fontname) {
+// 						fs.appendFile(source_folder + '/scss/fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
+// 					}
+// 					c_fontname = fontname;
+// 				}
+// 			}
+// 		}) // скобка зачем?
+// 	}
+// }
+// function cb() {}
+// exports.fontsStyle = fontsStyle;
+// и добавить в build
