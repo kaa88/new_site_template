07ad35a601264db1433652d2d5cc81cec6d4d753
Andrei <dre021088@gmail.com>
2022-12-21 16:19:34 +0800

    v.4.00

--- gulpfile.js
+++ gulpfile.js
@@ -1,101 +1,141 @@
-let isLiteBuild = isWP = false, load = {};
+let isLiteBuild = isWordPress = false, liteBuild_load = exportSettings = {};
 
-//////////////////////////////////////////////////
+// INFO //
 
-// Basic settings //
-let scriptsPrefix = 'website.'
+// npm i --save-dev *plugin*
+
+// npm i -g browser-sync
+// browser-sync start --proxy "localhost" --files "**/*" --no-notify
+
+// isLiteBuild:
+// - добавляет переменную (isLiteBuild), с которой могут работать html-сборщик и scss-препроцессор
+// - не минифицирует и не создает файлы .min.css и .min.js, не чистит html
+// - не копирует (без доп настроек) libs, service, media (кроме img)
+// - не сжимает изображения, но нарезает @1x и @2x
+// - favicon нарезает только S-размер
+// - не конвертирует шрифты, а копирует только базовый ttf; в scss отключает загрузку остальных шрифтов (чтобы не было ошибок в консоли), кроме iconfont
+
+// isWordPress:
+// - добавляет переменную (isWordPress), с которой могут работать html-сборщик и scss-препроцессор
+// - меняет название папки билда
+// - меняет части html кода на WordPress php-команды ... (в разработке)
+// - меняет расширения html на php, а так же название 'index' на 'home'
+// - копирует файлы шаблона для WordPress
+
+///////////////////////////////////////////////////////
+
+// Build settings //
+const 
+	scriptsPrefix = 'website.',
+	faviconSizes = { L: 270, M: 180, S: 32 },
+	jpgMinifyQuality = 80, // min 70 for good quality
+	pngMinifyQuality = 5; // recommended
+
+///////////////////////////////////////////////////////
 
 isLiteBuild = true
-// load.media = true
-// load.libs = true
-// load.service = true
-// isWP = true
+// liteBuild_load.media = true
+// liteBuild_load.libs = true
+// liteBuild_load.service = true
 
+// isWordPress = true
 
-if (isWP) isLiteBuild = false // important
+if (isWordPress) isLiteBuild = false // important
 
-//////////////////////////////////////////////////
+///////////////////////////////////////////////////////
 
 // Includes //
 const { src, dest } = require('gulp'),
 	gulp = require('gulp'),
-	fs = require('fs'),
-	streamqueue = require('streamqueue'),
 	browsersync = require('browser-sync').create(),
+	streamqueue = require('streamqueue'),
+	fs = require('fs'),
 	del = require('del'),
 	rename = require('gulp-rename'),
-	fileinclude = require('gulp-file-include'),
-	scss = require('gulp-sass')(require('sass')),
-	autoprefixer = require('gulp-autoprefixer'),
-	css_media_queries = require('gulp-group-css-media-queries'),
+	fileinclude = require('gulp-file-include'), // html & js
+	sass = require('gulp-sass')(require('sass')),
+	autoprefixer = require('gulp-autoprefixer'), // css
+	html_beautify = require('gulp-html-beautify'),
+	clean_html = require('gulp-htmlmin'),
 	clean_css = require('gulp-clean-css'),
-	uglify = require('gulp-uglify-es').default,
+	clean_js = require('gulp-uglify-es').default,
 	ttf2woff = require('gulp-ttf2woff'),
 	ttf2woff2 = require('gulp-ttf2woff2'),
 	fonter = require('gulp-fonter'),
 	imageresize = require('gulp-scale-images'),
-	readMetadata = require('gulp-scale-images/read-metadata'),
-	through = require('through2'), // for gulp-scale-images
+	readMetadata = require('gulp-scale-images/read-metadata'), // gulp-scale-images
+	through = require('through2'), // gulp-scale-images
 	imagemin = require('gulp-imagemin-changba');
 
-//////////////////////////////////////////////////
+	// Когда-то использовал: (оставить здесь, удалить из package)
+	// css_media_queries = require('gulp-group-css-media-queries')
+
+///////////////////////////////////////////////////////
 
 // Path //
-let $source = '#src';
+let $source = '_src';
 let $baseDir = {
 	html: $source + '/',
 	css: $source + '/css/',
 	js: $source + '/js/',
+	fonts: $source + '/fonts/',
+	img: $source + '/media/img/',
+	media: $source + '/media/',
 	data: $source + '/data/',
 	service: $source + '/service/',
 	libs: $source + '/libs/',
-	media: $source + '/media/',
-	img: $source + '/media/img/',
-	fonts: $source + '/fonts/',
 }
-let $project = isWP ? 'assets' : 'dist';
-let path = {
+let $exportSettings = {
+	src: $source + '/loaded_modules.js',
+	scss: $baseDir.css + '/gulp-settings.scss',
+}
+let $project = isWordPress ? 'assets' : 'dist';
+let $path = {
 	clean: ['./dist/', './assets/'],
 	watch: {
-		html: $source + '/**/*.html',
-		css: $baseDir.css + '**/*.scss',
+		settings: $exportSettings.src,
+		html: $baseDir.html + '**/*.html',
+		css: $baseDir.css + '*.css',
+		scss: $baseDir.css + '**/*.scss',
 		js: $baseDir.js + '**/*.js',
-		data: $baseDir.data + '**/*',
-		service: $baseDir.service + '**/*',
-		media: $baseDir.media + '**/*.{mp4,avi,webm,ogg,mp3,pdf,txt,doc,docx,xls,xlsx,ppt,pptx,rtf}',
-		img: $baseDir.img + '**/*.{jpg,png,svg,gif,ico,webp}',
 		fonts_otf: $baseDir.fonts + 'otf/*.otf',
 		fonts_ttf: $baseDir.fonts + '*.ttf',
+		img: $baseDir.img + '**/*.{jpg,png,svg,gif,ico,webp}',
+		media: $baseDir.media + '**/*.{mp4,avi,webm,ogg,mp3,pdf,txt,doc,docx,xls,xlsx,ppt,pptx,rtf}',
+		data: $baseDir.data + '**/*',
+		service: $baseDir.service + '**/*',
 	},
 	src: {
 		html: [$baseDir.html + '**/*.html', '!' + $baseDir.html + '**/#*.html', '!' + $baseDir.html + '**/parts/*'],
 		html_wp: [$baseDir.html + '**/*.html', '!' + $baseDir.html + '**/#*.html'],
-		css: $baseDir.css + '*style.scss',
+		css: $baseDir.css + '*.css',
+		scss: $baseDir.css + '*style.scss',
 		js: $baseDir.js + '*.js',
-		data: $baseDir.data + '**/*',
-		service: [$baseDir.service + '**/*', $baseDir.service + '.htaccess'],
-		libs: $baseDir.libs + '**/*',
-		media: [$baseDir.media + '**/*', '!' + $baseDir.media + 'img/**/*'],
+		fonts_otf: $baseDir.fonts + 'otf/*.otf',
+		fonts_ttf: $baseDir.fonts + '*.ttf',
 		img: $baseDir.img + '**/*',
 		img2x: $baseDir.img + '**/*@2x.*',
 		img_uncompressed: $baseDir.img + '**/$*',
 		img_favicon: $baseDir.img + 'favicon.png',
-		fonts_otf: $baseDir.fonts + 'otf/*.otf',
-		fonts_ttf: $baseDir.fonts + '*.ttf',
+		media: [$baseDir.media + '**/*', '!' + $baseDir.media + 'img/**/*'],
+		data: $baseDir.data + '**/*',
+		service: [$baseDir.service + '**/*', $baseDir.service + '.htaccess'],
+		libs: $baseDir.libs + '**/*',
 	},
 	build: {
-		root: $project + '/',
+		html: $project + '/',
 		css: $project + '/css/',
 		js: $project + '/js/',
-		data: $project + '/data/',
-		libs: $project + '/libs/',
-		media: $project + '/media/',
-		img: $project + '/media/img/',
 		fonts: $project + '/fonts/',
+		img: $project + '/media/img/',
+		media: $project + '/media/',
+		data: $project + '/data/',
+		service: $project + '/',
+		libs: $project + '/libs/',
 	}
 }
 
-//////////////////////////////////////////////////
+///////////////////////////////////////////////////////
 
 function cb() {}
 
@@ -107,140 +147,179 @@ function getExtendedDir(name, dir) {
 }
 
 function clean() {
-	return del(path.clean);
+	return del($path.clean);
 }
 
-//////////////////////////////////////////////////
+function getSettings() {
+	let settingsPath = './' + $exportSettings.src;
+	delete require.cache[require.resolve(settingsPath)];
+	exportSettings = require(settingsPath).settings; // export name
+	exportSettings.isLiteBuild = isLiteBuild;
+	exportSettings.isWordPress = isWordPress;
+	return Promise.resolve('ok');
+}
+
+function setCssSettings() {
+	let optionsStr = '// Do not change this file manually, Gulp will overwrite it. The main settings file is: ' + $exportSettings.src + '\r\n';
+	for(let key in exportSettings) {
+		optionsStr += '$' + key + ': ' + exportSettings[key] + ';\r\n';
+	}
+	fs.writeFile($exportSettings.scss, optionsStr, cb);
+	return Promise.resolve('ok');
+}
+
+///////////////////////////////////////////////////////
 
 function html(cb, file) {
-	let filepath = isWP ? path.src.html_wp : path.src.html, extDir = '';
+	let filepath = isWordPress ? $path.src.html_wp : $path.src.html, extDir = '';
 	if (file && !file.match(/\/#/)) {
-		if (isWP || !isWP && !file.match(/\/parts\//)) {
+		if (isWordPress || !isWordPress && !file.match(/\/parts\//)) {
 			filepath = file;
 			extDir = getExtendedDir('html', file);
 		}
 	}
 
 	let stream = src(filepath);
-	if (isWP)
+	if (isWordPress) {
 		stream = stream
-			.pipe(rename(function(path) {
-				if (path.basename == 'index') path.basename = 'home';
-				path.extname = '.php';
-			}));
-	else
+			.pipe(rename(function(file) {
+				if (file.basename == 'index') file.basename = 'home';
+				file.extname = '.php';
+			}))
+	}
+	else {
 		stream = stream
 			.pipe(fileinclude({
 				indent: true,
-				context: {light: isLiteBuild}
-			}));
+				context: exportSettings
+			}))
+	}
+
+	if (!isLiteBuild) {
+		stream = stream
+			.pipe(clean_html({
+				removeComments: true,
+				collapseWhitespace: true,
+				preserveLineBreaks: true,
+			}))
+			.pipe(html_beautify({
+				indent_with_tabs: true,
+				indent_size: 1,
+			}))
+	}
 	
 	return stream
-		.pipe(dest(path.build.root + extDir))
-		.pipe(browsersync.stream());
+		.pipe(dest($path.build.html + extDir))
+		.pipe(browsersync.stream())
 }
 
-//////////////////////////////////////////////////
+///////////////////////////////////////////////////////
 
-function css() {
-	let stream = src(path.src.css)
-		.pipe(scss({
-			outputStyle: 'expanded'
-		}))
+function css(cb, file) {
+	let filepath = file ? file : $path.src.css;
+	let extDir = file ? getExtendedDir('css', file) : '';
+	return src(filepath)
+		.pipe(clean_css())
+		.pipe(rename({ extname: '.min.css' }))
+		.pipe(dest($path.build.css + extDir))
+		.pipe(browsersync.stream())
+}
+
+function scss() {
+	let stream = src($path.src.scss)
+		.pipe(sass({ outputStyle: 'expanded' }))
 		.pipe(autoprefixer({
 			overrideBrowserslist: ['last 5 versions'],
 			cascade: true
 		}))
-		.pipe(css_media_queries())
-		.pipe(rename({
-			prefix: scriptsPrefix
-		}));
+		// .pipe(css_media_queries())
+		// отключил т.к. считаю, что медиа должны быть сразу за эл-том
+		.pipe(rename({ prefix: scriptsPrefix }))
 
-	if (!isLiteBuild)
+	if (!isLiteBuild) {
 		stream = stream
-			.pipe(dest(path.build.css))
+			.pipe(dest($path.build.css))
 			.pipe(clean_css())
-			.pipe(rename({
-				extname: '.min.css'
-			}));
+			.pipe(rename({ extname: '.min.css' }))
+	}
+	// пробовал объединить оригинал и .min в streamqueue, но rename меняет имя оригинала (т.к. копия объекта по ссылке), нужно ставить модуль gulp-clone наверное
+	// для чего streamqueue? в browsersync проходит инфа только о последнем файле (.min), и если в html подключен не.min, не обновляет страницу (типо style.css не менялся)
 
 	return stream
-		.pipe(dest(path.build.css))
-		.pipe(browsersync.stream());
+		.pipe(dest($path.build.css))
+		.pipe(browsersync.stream())
 }
 
-//////////////////////////////////////////////////
+///////////////////////////////////////////////////////
 
 function js() {
-	let stream = src(path.src.js)
-		.pipe(fileinclude())
-		.pipe(rename({
-			prefix: scriptsPrefix
-		}));
+	let stream = src($path.src.js)
+		.pipe(fileinclude({
+			indent: true,
+			context: exportSettings
+		}))
+		.pipe(rename({ prefix: scriptsPrefix }))
 
-	if (!isLiteBuild)
+	if (!isLiteBuild) {
 		stream = stream
-			.pipe(dest(path.build.js))
-			.pipe(uglify())
-			.pipe(rename({
-				extname: '.min.js'
-			}));
-
+			.pipe(dest($path.build.js))
+			.pipe(clean_js())
+			.pipe(rename({ extname: '.min.js' }))
+	}
 	return stream
-		.pipe(dest(path.build.js))
-		.pipe(browsersync.stream());
+		.pipe(dest($path.build.js))
+		.pipe(browsersync.stream())
 }
 
-//////////////////////////////////////////////////
+///////////////////////////////////////////////////////
 
 function data(cb, file) {
-	let filepath = file ? file : path.src.data;
+	let filepath = file ? file : $path.src.data;
 	let extDir = file ? getExtendedDir('data', file) : '';
 	return src(filepath)
-		.pipe(dest(path.build.data + extDir))
-		.pipe(browsersync.stream());
+		.pipe(dest($path.build.data + extDir))
+		.pipe(browsersync.stream())
 }
 
-//////////////////////////////////////////////////
+///////////////////////////////////////////////////////
 
 function service(cb, file) {
-	if (!isLiteBuild || load.service) {
-		let filepath = file ? file : path.src.service;
+	if (!isLiteBuild || liteBuild_load.service) {
+		let filepath = file ? file : $path.src.service;
 		let extDir = file ? getExtendedDir('service', file) : '';
 		return src(filepath, {
 				allowEmpty: true // .htaccess error fix
 			})
-			.pipe(dest(path.build.root + extDir))
-			.pipe(browsersync.stream());
+			.pipe(dest($path.build.service + extDir))
+			.pipe(browsersync.stream())
 	}
 	else cb();
 }
 
-//////////////////////////////////////////////////
+///////////////////////////////////////////////////////
 
 function libs(cb) {
-	if (!isLiteBuild || load.libs)
-		return src(path.src.libs)
-			.pipe(dest(path.build.libs));
+	if (!isLiteBuild || liteBuild_load.libs) {
+		return src($path.src.libs)
+			.pipe(dest($path.build.libs));
+	}
 	else cb();
 }
 
-//////////////////////////////////////////////////
+///////////////////////////////////////////////////////
 
 function media(cb, file) {
-	if (!isLiteBuild || load.media) {
-		let filepath = file ? file : path.src.media;
+	if (!isLiteBuild || liteBuild_load.media) {
+		let filepath = file ? file : $path.src.media;
 		let extDir = file ? getExtendedDir('media', file) : '';
 		return src(filepath)
-			.pipe(dest(path.build.media + extDir))
+			.pipe(dest($path.build.media + extDir))
 			.pipe(browsersync.stream());
 	}
 	else cb();
 }
 
-//////////////////////////////////////////////////
-
-const faviconSizes = { L: 270, M: 180, S: 32 }
+///////////////////////////////////////////////////////
 
 function scaleFavicon(file, size) {
 	file = file.clone();
@@ -291,17 +370,18 @@ function renameImage2x(file, _, cb) {
 }
 
 function images(cb, filepath) {
-	let images = [path.src.img, '!' + path.src.img2x, '!' + path.src.img_uncompressed, '!' + path.src.img_favicon];
-	let images2x = [path.src.img2x, '!' + path.src.img_uncompressed];
-	let uncompressed = path.src.img_uncompressed;
-	let favicon = path.src.img_favicon;
+	let images = [$path.src.img, '!' + $path.src.img2x, '!' + $path.src.img_uncompressed, '!' + $path.src.img_favicon];
+	let images2x = [$path.src.img2x, '!' + $path.src.img_uncompressed];
+	let uncompressed = $path.src.img_uncompressed;
+	let favicon = $path.src.img_favicon;
 
 	function getFaviconStream(filepath = favicon) {
-		if (isLiteBuild)
+		if (isLiteBuild) {
 			return src(filepath)
 				.pipe(through.obj(scaleFaviconS))
 				.pipe(imageresize(renameFaviconS));
-		else
+		}
+		else {
 			return streamqueue(
 				{ objectMode: true },
 				src(filepath)
@@ -313,7 +393,8 @@ function images(cb, filepath) {
 				src(filepath)
 					.pipe(through.obj(scaleFaviconS))
 					.pipe(imageresize(renameFaviconS))
-			);
+			)
+		}
 	}
 
 	let stream, extDir = '';
@@ -331,7 +412,7 @@ function images(cb, filepath) {
 		// uncompressed
 		if (filepath.match(/\/\$/))
 			return stream
-				.pipe(dest(path.build.img + extDir))
+				.pipe(dest($path.build.img + extDir))
 				.pipe(browsersync.stream());
 		// favicon
 		if (filepath.match(/favicon\.png/))
@@ -350,47 +431,45 @@ function images(cb, filepath) {
 		)
 	}
 	// common
-	if (!isLiteBuild)
+	if (!isLiteBuild) {
 		stream = stream
 			.pipe(imagemin([
-				imagemin.mozjpeg({ quality: 80, progressive: true }),
-				imagemin.optipng({ optimizationLevel: 5 })
+				imagemin.mozjpeg({ quality: jpgMinifyQuality, progressive: true }),
+				imagemin.optipng({ optimizationLevel: pngMinifyQuality })
 			]));
+	}
 
 	// if all files, add $-files
 	if (!filepath) stream = stream.pipe(src(uncompressed));
 
 	return stream
-		.pipe(dest(path.build.img + extDir))
+		.pipe(dest($path.build.img + extDir))
 		.pipe(browsersync.stream());
 }
 
-//////////////////////////////////////////////////
+///////////////////////////////////////////////////////
 
-function fonts(cb, filepath = path.src.fonts_ttf) {
+function fonts(cb, filepath = $path.src.fonts_ttf) {
 	let stream;
-	if (!isLiteBuild)
+	if (!isLiteBuild) {
 		stream = streamqueue(
 			{ objectMode: true },
 			src(filepath)
-				.pipe(ttf2woff({
-					clone: true
-				})),
+				.pipe(ttf2woff({ clone: true })),
 			src(filepath)
 				.pipe(ttf2woff2())
 		)
+	}
 	else stream = src(filepath);
 
 	return stream
-		.pipe(dest(path.build.fonts))
-		.pipe(browsersync.stream());
+		.pipe(dest($path.build.fonts))
+		.pipe(browsersync.stream())
 }
 
-function otf(cb, filepath = path.src.fonts_otf) {
+function otf2ttf(cb, filepath = $path.src.fonts_otf) {
 	return src(filepath)
-		.pipe(fonter({
-			formats: ['ttf']
-		}))
+		.pipe(fonter({ formats: ['ttf'] }))
 		.pipe(dest([$source + '/fonts/']));
 }
 
@@ -398,7 +477,7 @@ function fontsStyle() {
 	let file_content = fs.readFileSync($source + '/css/fontscript.scss');
 	if (file_content == '') {
 		fs.writeFile($source + '/css/fontscript.scss', '', cb);
-		return fs.readdir(path.build.fonts, function (err, items) {
+		fs.readdir($path.build.fonts, function (err, items) {
 			if (items) {
 				let c_fontname;
 				for (let i = 0; i < items.length; i++) {
@@ -413,20 +492,19 @@ function fontsStyle() {
 			}
 		})
 	}
+	return Promise.resolve('ok');
 }
 
-//////////////////////////////////////////////////
+///////////////////////////////////////////////////////
 
 function browserSyncInit() {
 	setTimeout(() => {
 		browsersync.init({
-			server: {
-				baseDir: './' + $project + '/'
-			},
+			server: { baseDir: './' + $project + '/' },
 			port: 3000,
 			notify: false
 		})
-	}, 2000)
+	}, 1000)
 }
 
 function watchFiles() {
@@ -439,29 +517,37 @@ function watchFiles() {
 			f(undefined, fixPath(path));
 		});
 	}
-	gulp.watch([path.watch.css], css).on('change', browsersync.reload);
-	gulp.watch([path.watch.js], js);
-	conditionalWatch(gulp.watch([path.watch.html]), html);
-	conditionalWatch(gulp.watch([path.watch.data]), data);
-	conditionalWatch(gulp.watch([path.watch.service]), service);
-	conditionalWatch(gulp.watch([path.watch.media]), media);
-	conditionalWatch(gulp.watch([path.watch.img]), images);
-	conditionalWatch(gulp.watch([path.watch.fonts_ttf]), fonts);
-	conditionalWatch(gulp.watch([path.watch.fonts_otf]), otf);
+	gulp.watch([$path.watch.settings], gulp.series(getSettings, setCssSettings, gulp.parallel(html, scss, js)));
+	gulp.watch([$path.watch.scss], scss);//.on('change', browsersync.reload);
+	gulp.watch([$path.watch.js], js);
+	conditionalWatch(gulp.watch([$path.watch.html]), html);
+	conditionalWatch(gulp.watch([$path.watch.css]), css);
+	conditionalWatch(gulp.watch([$path.watch.data]), data);
+	conditionalWatch(gulp.watch([$path.watch.service]), service);
+	conditionalWatch(gulp.watch([$path.watch.media]), media);
+	conditionalWatch(gulp.watch([$path.watch.img]), images);
+	conditionalWatch(gulp.watch([$path.watch.fonts_ttf]), fonts);
+	conditionalWatch(gulp.watch([$path.watch.fonts_otf]), otf2ttf);
 }
 
 let start = gulp.parallel(
 	watchFiles,
 	gulp.series(
 		clean,
-		otf,
+		getSettings,
+		setCssSettings,
+		otf2ttf,
 		fonts,
-		gulp.parallel(html, css, js, data, service, libs, media, images),
-		gulp.parallel(fontsStyle, browserSyncInit),
+		fontsStyle,
+		gulp.parallel(data, service, libs, media, images),
+		gulp.parallel(html, css, scss, js),
+		browserSyncInit
 	)
-);
+)
 
-exports.otf = otf;
+exports.setCssSettings = setCssSettings;
+exports.getSettings = getSettings;
+exports.otf2ttf = otf2ttf;
 exports.fonts = fonts;
 exports.fontsStyle = fontsStyle;
 exports.images = images;
@@ -471,14 +557,7 @@ exports.service = service;
 exports.data = data;
 exports.js = js;
 exports.css = css;
+exports.scss = scss;
 exports.html = html;
 exports.start = start;
 exports.default = start;
-
-//////////////////////////////////////////////////
-
-// Info
-// npm i --save-dev *plugin*
-
-// npm i -g browser-sync
-// browser-sync start --proxy "localhost" --files "**/*" --no-notify
